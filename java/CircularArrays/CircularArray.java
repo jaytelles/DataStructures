22a61439c191266c60c7a475efb6e59b7436265d
root <jaytelles24@gmail.com>
2018-02-24 17:06:49 -0500

    added append method

--- java/CircularArrays/CircularArray.java
+++ java/CircularArrays/CircularArray.java
@@ -1,22 +1,42 @@
-class CircularArray(){
+class CircularArray{
     private int start;
     private int end;
-    private int size;
+    private int maxSize;
+    private int currentSize;
     private Object[] array;
     
-    public void CircularArray(int size){
+    public CircularArray(int size){
         start = 0;
         end = 0;
-        this.size = arraySize;
-        array = new Object[this.size];
+        maxSize = size;
+        currentSize = 0;
+        array = new Object[maxSize];
+    }
+    
+    public boolean append(Object element){
+        //if ((end + 1) % maxSize == start) {
+        if (currentSize + 1 > maxSize) {
+            return false;
+        }
+        
+        array[end] = element;
+        end = (end + 1) % maxSize;
+        currentSize++;
+        
+        System.out.print("start: " + start);
+        System.out.print("    end: " + end);
+        System.out.println("    " + this);
+        
+        return true;
     }
     
     public String toString(){
         String retStr = "";
-        for(int k=0; k<this.size; k++){
-            retStr += this.array[k];
+        for(int k=0; k<maxSize; k++){
+            retStr += array[k];
             retStr += " ";
         }
         retStr += "\b";
+        return retStr;
     }
 }
--- java/CircularArrays/Tester.java
+++ java/CircularArrays/Tester.java
@@ -1,14 +1,20 @@
-import "CircularArray.java"
 public class Tester {
     public static void main(String[] args){
-        Object[] array = new Object[10]; //static array initialized for you :)
+        CircularArray xxx = new CircularArray(5);
+        /*System.out.println(xxx.append(0));
+        System.out.println(xxx.append(1));
+        System.out.println(xxx.append(2));
+        System.out.println(xxx.append(3));
+        System.out.println(xxx.append(4));
+        System.out.println(xxx.append(5));*/
+        xxx.append(0);
+        xxx.append(1);
+        xxx.append(2);
+        xxx.append(3);
+        xxx.append(4);
+        xxx.append(5);
+        xxx.append(6);
         
         
-        int x = 1;
-        array[0] = x;
-        
-        for (int k=0; k<10; k++){
-            System.out.println(array[k]);
-        }
     }
 }
