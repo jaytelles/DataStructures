5fa78ca3ed4b57eb84e0eb5448ed04708e95b7ff
root <jaytelles24@gmail.com>
2018-02-24 17:19:03 -0500

    updates to the test script

--- java/CircularArrays/Tester.java
+++ java/CircularArrays/Tester.java
@@ -1,20 +1,12 @@
 public class Tester {
     public static void main(String[] args){
         CircularArray xxx = new CircularArray(5);
-        /*System.out.println(xxx.append(0));
-        System.out.println(xxx.append(1));
-        System.out.println(xxx.append(2));
-        System.out.println(xxx.append(3));
-        System.out.println(xxx.append(4));
-        System.out.println(xxx.append(5));*/
-        xxx.append(0);
-        xxx.append(1);
-        xxx.append(2);
-        xxx.append(3);
-        xxx.append(4);
-        xxx.append(5);
-        xxx.append(6);
-        
+        System.out.println(xxx.prepend(0));
+        System.out.println(xxx.prepend(1));
+        System.out.println(xxx.prepend(2));
+        System.out.println(xxx.prepend(3));
+        System.out.println(xxx.prepend(4));
+        System.out.println(xxx.prepend(5));
         
     }
 }
