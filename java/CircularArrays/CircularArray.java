b421b4ec68ee5a6c5f5dbd4640c03d791c263a7f
root <jaytelles24@gmail.com>
2018-03-07 21:05:35 -0500

    added decrementIndex and incrementIndex helper functions and started implementing pop(index)

--- java/CircularArrays/CircularArray.java
+++ java/CircularArrays/CircularArray.java
@@ -14,19 +14,19 @@ class CircularArray{
     }
     
     public boolean append(Object element){
-        if (currentSize + 1 > maxSize) {
+        if (currentSize + 1 > maxSize || element == null) {
             return false;
         }
         
         array[end] = element;
-        end = (end + 1) % maxSize; //this may cause issues when it comes time to remove elements
+        end = incrementIndex(end); //this may cause issues when it comes time to remove elements
         currentSize++;
         
         return true;
     }
     
     public boolean prepend(Object element){
-        if (currentSize + 1 > maxSize) {
+        if (currentSize + 1 > maxSize || element == null) {
             return false;
         }
         
@@ -41,21 +41,74 @@ class CircularArray{
     }
     
     public Object pop(){
-        if(currentSize==0){
+        if(currentSize == 0){
             return null;
         }
         
-        if(end > 0){
-            end--;
-        } else {
-            end = maxSize - 1;
-        }
+        end = decrementIndex(end);
         
         Object retval = array[end];
         currentSize--;
         
         return retval;
     }
+    
+    public Object pop(int index){
+        //add a return null if index isnt in the active segment?\
+        //build available indices by taking the different between maxSize-end and maxSize-start?
+        if (currentSize == 0 || index < 0 || index > maxSize){
+            return null;
+        }
+        
+        Object ret = array[index];
+        
+        if (index == start){
+            start = incrementIndex(start);
+        }
+        
+        if (index == end){
+            end = decrementIndex(end);
+        }
+
+        //if the index is closer to the start than the end
+        if(Math.abs(start - index) < Math.abs(end - index)){
+            int k = index;
+            //while()
+        }
+        //if the index is closer to the end than the start
+        else {
+            int k = index;
+            while (k != end){
+                int next = incrementIndex(k);
+                array[k] = array[next];
+                incrementIndex(k);
+                end = decrementIndex(end);
+            }
+        }
+        
+        currentSize--;
+        return ret;
+    }
+    
+    private int decrementIndex(int index){
+        if (index > 0){
+            index--;
+        } else {
+            index = maxSize - 1;
+        }
+        return index;
+    }
+    
+    private int incrementIndex(int index){
+        index = (index + 1) % maxSize;
+        return index;
+    }
+    
+    //deep copy
+    //pop at index
+    //search?
+    //sort?
+    
     
     public void printInfo() {
         System.out.print("start: " + start);
